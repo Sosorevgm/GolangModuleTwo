e3cbfac43094d76d2070920f5b42678ef7008af2
Sosorevgm <sosorevgm@gmail.com>
2021-02-19 14:41:31 +0300

    Homework one

--- homework_one/main.go
+++ homework_one/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"GolangModuleTwo/homework_one/custom_errors"
+	"fmt"
+)
+
 func main() {
-	
+	taskOne()
+
+	taskTwo := func() {
+		customErr := custom_errors.New("Some error occurred")
+		fmt.Print(customErr)
+	}
+	taskTwo()
+
+	taskThree()
 }
